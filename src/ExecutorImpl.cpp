daace57f26db6476134e45984a9d155827c0169c
T0W3R-Su <runxunsu@gmail.com>
2024-11-22 22:19:16 +0800

    feat: add initial and default pose and pass test cases. Add move instruction and pass test cases

--- src/ExecutorImpl.cpp
+++ src/ExecutorImpl.cpp
@@ -7,12 +7,31 @@ Executor* Executor::NewExecutor(const Pose& initialPose) noexcept
     return new ExecutorImpl(initialPose);
 }
 
-ExecutorImpl::ExecutorImpl(const Pose& initialPose) noexcept
+ExecutorImpl::ExecutorImpl(const Pose& initialPose) noexcept : pose(initialPose)
 {
 }
 
 void ExecutorImpl::Execute(const std::string& instruction) noexcept
 {
+    for (const auto ins : instruction) {
+        switch (ins) {
+        case 'M':
+            switch (pose.heading) {
+            case 'N':
+                pose.y++;
+                break;
+            case 'E':
+                pose.x++;
+                break;
+            case 'S':
+                pose.y--;
+                break;
+            case 'W':
+                pose.x--;
+                break;
+            }
+        }
+    }
 }
 
 const Pose ExecutorImpl::Query(void) const noexcept
--- tests/ExecutorTest.cpp
+++ tests/ExecutorTest.cpp
@@ -25,7 +25,7 @@ TEST(ExecutorTest, should_return_initial_pose_when_no_instruction)
 TEST(ExecutorTest, should_return_default_pose_when_no_init_and_no_instruction)
 {
     // Arrange
-    std::unique_ptr<Executor> executor(Executor::NewExecutor({}));
+    std::unique_ptr<Executor> executor(Executor::NewExecutor());
 
     // Act
 
@@ -33,4 +33,57 @@ TEST(ExecutorTest, should_return_default_pose_when_no_init_and_no_instruction)
     const Pose target{0, 0, 'N'};
     ASSERT_EQ(executor->Query(), target);
 }
+
+TEST(ExecutorTest, should_return_x_plus_1_when_instruction_is_M_and_heading_is_E)
+{
+    // Arrange
+    std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'E'}));
+
+    // Act
+    executor->Execute("M");
+
+    // Assert
+    const Pose target{1, 0, 'E'};
+    ASSERT_EQ(executor->Query(), target);
+}
+
+TEST(ExecutorTest, should_return_x_minus_1_when_instruction_is_M_and_heading_is_W)
+{
+    // Arrange
+    std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'W'}));
+
+    // Act
+    executor->Execute("M");
+
+    // Assert
+    const Pose target{-1, 0, 'W'};
+    ASSERT_EQ(executor->Query(), target);
+}
+
+TEST(ExecutorTest, should_return_y_plus_1_when_instruction_is_M_and_heading_is_N)
+{
+    // Arrange
+    std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'N'}));
+
+    // Act
+    executor->Execute("M");
+
+    // Assert
+    const Pose target{0, 1, 'N'};
+    ASSERT_EQ(executor->Query(), target);
+}
+
+TEST(ExecutorTest, should_return_y_minus_1_when_instruction_is_M_and_heading_is_S)
+{
+    // Arrange
+    std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'S'}));
+
+    // Act
+    executor->Execute("M");
+
+    // Assert
+    const Pose target{0, -1, 'S'};
+    ASSERT_EQ(executor->Query(), target);
+}
+
 }  // namespace adas
